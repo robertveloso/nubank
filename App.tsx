5d9f409bba45927b9d6b149ca06b4c1e6057e85f
ROBERT VELOSO <veloso@robert.local>
2020-06-02 22:07:28 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import App from "./src";
+
+export default function Aspp() {
+  return <App />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/pages/Main/index.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { Animated } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+import Header from "components/Header";
+import Tabs from "components/Tabs";
+import Menu from "components/Menu";
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+  SafeAreaView,
+} from "./styles";
+
+export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 600 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 600 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
+  return (
+    <SafeAreaView>
+      <Container>
+        <Header />
+        <Content>
+          <Menu translateY={translateY} />
+
+          <PanGestureHandler
+            onGestureEvent={animatedEvent}
+            onHandlerStateChange={onHandlerStateChanged}
+          >
+            <Card
+              style={{
+                transform: [
+                  {
+                    translateY: translateY.interpolate({
+                      inputRange: [-350, 0, 420],
+                      outputRange: [-50, 0, 420],
+                      extrapolate: "clamp",
+                    }),
+                  },
+                ],
+              }}
+            >
+              <CardHeader>
+                <MaterialIcons name="attach-money" size={28} color="#666" />
+                <MaterialIcons name="visibility-off" size={28} color="#666" />
+              </CardHeader>
+              <CardContent>
+                <Title>Saldo disponível</Title>
+                <Description>R$ 197.611,65</Description>
+              </CardContent>
+              <CardFooter>
+                <Annotation>
+                  Transferência de R$ 400,00 recebida de Veloso Digital hoje às
+                  06:00h
+                </Annotation>
+              </CardFooter>
+            </Card>
+          </PanGestureHandler>
+        </Content>
+
+        <Tabs translateY={translateY} />
+      </Container>
+    </SafeAreaView>
+  );
+}
